852fcee6492cea5412a6c6f5882c1975a2c4de96
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2023-11-23 22:20:42 +0900

    @iwan adding content to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import Image from "next/image";
-
+import { Outfit } from "next/font/google";
+import Button from "./components/Button";
+const outfit = Outfit({ subsets: ["latin"], weight: ["300"] });
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <section className="flex p-24">
+        <h1 className="black-96 uppercase basis-[60%]">modern art gallery</h1>
+        <div>
+          <p className={`${outfit.className} light-22`}>
+            The arts in the collection of the Modern Art Gallery all started
+            from a spark of inspiration. Will these pieces inspire you? Visit us
+            and find out.
+          </p>
+          <Button />
+        </div>
+      </section>
+    </main>
+  );
 }
