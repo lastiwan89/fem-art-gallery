36a0df0a65116249ccbda2de986035cd07835052
lastiwan89 <59128819+lastiwan89@users.noreply.github.com>
2023-11-23 22:25:51 +0900

    @iwan button with some improvements

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React, { useState, createContext } from "react";
 import Image from "next/image";
+import Link from "next/link";
 const ButtonContext = createContext<any>(null);
 export default function Button() {
   const [isOpen, setIsOpen] = useState<boolean>(true);
@@ -8,25 +10,26 @@ export default function Button() {
     <ButtonContext.Provider value={isOpen}>
       <button onClick={() => setIsOpen((prev) => !prev)}>
         {isOpen ? (
-          <div>
-            <p>our location</p>
+          <Link href={`/location`}>
+            <p className="uppercase">our location</p>
             <Image
               src="/icon-arrow-right.svg"
               width={24}
               height={24}
               alt="arrow right"
             />
-          </div>
+          </Link>
         ) : (
-          <div>
+          <Link href={`/`}>
             <Image
+              className="bg-black p-6"
               src="/icon-arrow-left.svg"
               width={24}
               height={24}
               alt="arrow left"
             />
-            <p>our location</p>
-          </div>
+            <p className="uppercase px-8 py-6 bg-orange-300">our location</p>
+          </Link>
         )}
       </button>
     </ButtonContext.Provider>
